5bd1ca47e8881685e4184811528db635c65deadc
Shivanshu <baranwalshivu542@gmail.com>
2023-11-03 21:37:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'  
+import Priority from './components/Priority'
+import Status from './components/Status'
+import UserContent from './components/UserContent'
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    <Navbar/>
+    <div className="containers">
+      <Routes>
+        {/* <Priority/> */}
+        <Route exact path="/" element={<Status/>} />
+        <Route exact path="/priority" element={<Priority/>} />
+        <Route exact path="/user" element={<UserContent/>} />
+
+      </Routes>
+        </div>
+    </Router>
+    </>
+
   );
 }
 
